5871aa88add333c020c5efa9de95b68f2f6e1f8d
AILyricEditor <ailyriceditor@gmail.com>
2025-07-24 00:31:07 -0400

    Made the expansion of the current song player more smooth

--- app/components/CurrentSong/CurrentSong.jsx
+++ app/components/CurrentSong/CurrentSong.jsx
@@ -15,6 +15,8 @@ export default function CurrentSong() {
 	const { currentSong, status, controls } = useCurrentSong();
 	const [isExpanded, setIsExpanded] = useState(false);
 	const [isExpanding, setIsExpanding] = useState(false);
+	const [arrowUp, setArrowUp] = useState(false);
+	const [active, setActive] = useState(false);
 	const pathname = usePathname();
 
 	let timeout;
@@ -22,6 +24,7 @@ export default function CurrentSong() {
 	const startY = useRef(null);
 
 	function handleTouchStart(e) {
+		e.preventDefault();
 		startY.current = e.touches[0].clientY;
 		setIsExpanding(true);
 	}
@@ -33,26 +36,39 @@ export default function CurrentSong() {
 		if (deltaY < -10) {
 			// SWIPE UP
 			setIsExpanded(true);
+			setArrowUp(true);
 		} else if (deltaY > 10) {
 			// SWIPE DOWN
-			setIsExpanded(false);
+			setIsExpanded(false)
 		}
 		setIsExpanding(false);
 	}
 
 	useEffect(() => {
-		if (isExpanded && status.isControlling === false) {
+		// if (isExpanded && status.isControlling === false) {
+		// 	// timeout = setTimeout(() => {
+		// 	// 	setIsExpanded(false);
+		// 	// }, 3000);
+		// } else if (status.isControlling === true) {
+		// 	if (timeout) clearTimeout(timeout);
+		// }
+
+		if (arrowUp) {
 			timeout = setTimeout(() => {
-				setIsExpanded(false);
-			}, 3000);
-		} else if (status.isControlling === true) {
-			if (timeout) clearTimeout(timeout);
+				setArrowUp(false);
+			}, 1300);
+		}
+
+		if (active) {
+			timeout = setTimeout(() => {
+				setActive(false);
+			}, 500);
 		}
 
 		return () => {
 			if (timeout) clearTimeout(timeout);
 		}
-	}, [isExpanded, status.isControlling]);
+	}, [isExpanded, status.isControlling, arrowUp, active]);
 
 	function openSongPage() {
 		controls.openCurrent();
@@ -60,6 +76,32 @@ export default function CurrentSong() {
 
 	return (
 		<>
+			{currentSong ? <div className={styles.topTarget} onClick={() => {
+					setArrowUp(true);
+					if (arrowUp) {
+						setActive(true);
+						setIsExpanded(!isExpanded);
+					}
+				}}
+				style={{ 
+					height: isExpanded ? '140px' : '110px',
+				}}
+				onTouchStart={handleTouchStart}
+				onTouchEnd={handleTouchEnd}
+			>
+				<div 
+				className={styles.arrow} 
+				style={{
+					top: arrowUp ? '0px' : '75%', 
+					opacity: arrowUp ? '1' : '0', 
+					visibility: arrowUp ? 'visible' : 'hidden',
+					rotate: isExpanded ? '180deg' : '0deg',
+					borderColor: active ? 'var(--accent2)' : 'var(--border-color)',
+					scale: active ? '1.1' : '1',
+				}}>
+					<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z"/></svg>
+				</div> 
+			</div> : null}
 			<div 
 				className={`
 					${styles.currentSong} 
@@ -70,6 +112,7 @@ export default function CurrentSong() {
 					${status.pendCurrentPage && styles.hidden}
 				`} 
 				style={{
+					// boxShadow: arrowUp || isExpanded ? '0 -20px 25px 15px rgba(0, 0, 0, 0.73)' : '0 0 25px 15px rgba(0, 0, 0, 0.411)',
 					touchAction: 'pan-y',
 					WebkitTapHighlightColor: 'transparent',
 				}}
@@ -79,12 +122,12 @@ export default function CurrentSong() {
 				onTouchStart={handleTouchStart}
 				onTouchEnd={handleTouchEnd}
 			>
-				<div 
+				{/* <div 
 					className={styles.handle}
 					style={{
 						width: isExpanding ? '50px' : '35px',
 					}} 
-				/>
+				/> */}
 				{currentSong && <>
 					<SongCover 
 						className={`${styles.songCover} pointer`}
