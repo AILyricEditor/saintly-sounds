3cd5ea584521aa91dbd4cdff62093d647bf09368
AILyricEditor <ailyriceditor@gmail.com>
2025-08-16 15:02:37 -0400

    Fixed a bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  basePath: '/repo-name', // your repo name
-  assetPrefix: '/repo-name', // ensures public assets resolve correctly
+  basePath: '/saintly-sounds', // your repo name
+  assetPrefix: '/saintly-sounds', // ensures public assets resolve correctly
 };
