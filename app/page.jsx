be68afea109594cbc0ac3e7d42b730e4af8a1841
AILyricEditor <ailyriceditor@gmail.com>
2026-03-30 23:07:42 -0400

    Testing...

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,7 @@ export default function Home() {
     // TODO: Add your HTML here
     <main>
 			<h1>Home</h1>
+      <p>TEST</p>
 			<p>There is no content here yet. Check out the <Link href={"/music"}>Music</Link> page!</p>
 		</main>
   );
